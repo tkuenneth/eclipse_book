623ddac7704494317b6c891e89ea6cf68106debf
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2018-05-20 09:41:49 +0200

    started work on chapters 4 and 4

---
+++ 04/41/src/com/thomaskuenneth/eclipse_book/Fakultaet.java
@@ -0,0 +1,18 @@
+package com.thomaskuenneth.eclipse_book;
+
+public class Fakultaet {
+
+	public static int fakultaet(int zahl) {
+		int ergebnis = 1;
+		if (zahl > 1) {
+			ergebnis = zahl * fakultaet(zahl - 1);
+		}
+		return ergebnis;
+	}
+
+	public static void main(String[] args) {
+		int zahl = Integer.parseInt(args[0]);
+		int ergebnis = fakultaet(zahl);
+		System.out.print(zahl + "! = " + ergebnis);
+	}
+}
