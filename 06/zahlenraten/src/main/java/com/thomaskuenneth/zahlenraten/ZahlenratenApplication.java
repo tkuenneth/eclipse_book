841112ae8f7191683ee025184bfbca9bd8d6e61a
Thomas Kuenneth <thomas@thomaskuenneth.com>
2018-08-17 08:09:42 +0200

    new project added

---
+++ 06/zahlenraten/src/main/java/com/thomaskuenneth/zahlenraten/ZahlenratenApplication.java
@@ -0,0 +1,12 @@
+package com.thomaskuenneth.zahlenraten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZahlenratenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZahlenratenApplication.class, args);
+	}
+}
